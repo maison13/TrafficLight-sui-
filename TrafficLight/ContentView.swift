fa9996b0cd63ffc77c4bc7e4f99a1b8a5dfcd958
Vladimir <smile_friend@mail.ru>
2023-06-11 00:11:27 +0300

    Создание цикличной логики работы.

--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     @State private var startButton = "START"
     @State private var lightsOn: CGFloat = 1
     @State private var lightsOff: CGFloat = 0.3
-    @State private var currentLight: Color = .red
+    @State private var indexOfCircle = -1
     
     var body: some View {
         ZStack{
@@ -19,14 +19,17 @@ struct ContentView: View {
                 .ignoresSafeArea()
             VStack {
                 
-                CircleView(color: .red, opacity: lightsOff)
-                CircleView(color: .orange, opacity: lightsOff)
-                CircleView(color: .green, opacity: lightsOff)
+                CircleView(color: .red, opacity: indexOfCircle == 0 ? lightsOn : lightsOff)
+                CircleView(color: .orange, opacity: indexOfCircle == 1 ? lightsOn : lightsOff)
+                CircleView(color: .green, opacity: indexOfCircle == 2 ? lightsOn : lightsOff)
+                
                 Spacer()
+                
                 Button(action: {
                     if startButton == "START" {
                         startButton = "NEXT"
                     }
+                indexOfCircle = (indexOfCircle + 1) % 3
                     
                 }, label: {Text(startButton)
                         .font(.title)
