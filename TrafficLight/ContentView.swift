ff24fa0fef3faf56934125f0d1095d9fa99f53a8
Vladimir <smile_friend@mail.ru>
2023-06-10 23:04:32 +0300

    create Circle

--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var startButton = "START"
+    @State private var lightsOn: CGFloat = 1
+    @State private var lightsOff: CGFloat = 0.3
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color(.black)
+                .ignoresSafeArea()
+            VStack {
+                
+                Circle()
+                    .foregroundColor(.red)
+                    .frame(width: 150, height: 150)
+                    .overlay(Circle().stroke(.white, lineWidth: 4))
+                    .shadow(radius: 10)
+                
+                Circle()
+                    .foregroundColor(.orange)
+                    .frame(width: 150, height: 150)
+                    .overlay(Circle().stroke(.white, lineWidth: 4))
+                    .shadow(radius: 10)
+                    .opacity(lightsOff)
+                Circle()
+                    .foregroundColor(.green)
+                    .frame(width: 150, height: 150)
+                    .overlay(Circle().stroke(.white, lineWidth: 4))
+                    .shadow(radius: 10)
+                    .opacity(lightsOff)
+                Spacer()
+                Button(action: {
+                    if startButton == "START" {
+                        startButton = "NEXT"
+                    }
+                    
+                    
+                }, label: {Text(startButton)})
+            }
+            .padding()
+            
+            
+            
+            
         }
-        .padding()
     }
 }
 
