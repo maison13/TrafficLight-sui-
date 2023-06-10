48724e28acde2fbb8c4d69f62ba6961f8c1eaf59
Vladimir <smile_friend@mail.ru>
2023-06-10 23:51:42 +0300

    Create Button and CirlceView

---
+++ TrafficLight/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  TrafficLight
+//
+//  Created by Vovo on 10.06.2023.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
